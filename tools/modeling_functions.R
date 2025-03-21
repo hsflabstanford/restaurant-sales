6a49aee1003c989e5f19e8f4c263506d28683205
jaredwins99 <jaredwins99@gmail.com>
2025-03-21 18:10:43 -0400

    removed ts for inputting data to model, added plotting predictions function

--- tools/modeling_functions.R
+++ tools/modeling_functions.R
@@ -14,134 +14,134 @@ library(tscount) # tsglm
 library(MASS) # glm.nb
 library(bayesforecast) # ACF, PACF
 
-
 ## ===== Main Modeling Functions: INGARCH =====
 
 # 1. Fits and returns a NB model with AR terms and Mean lag teams
 fit_nbar_model <- function(df, outcome, predictors, ar_lags=c(), mean_lags=c()) {
-  
+
   # tsglm takes time series
-  outcome_ts <- ts(df[[outcome]])
+  outcome_ts <- df[[outcome]]
   # Check if predictors are provided
   xreg <- if (!is.null(predictors)) {model.matrix(~ . - 1, data = df[, predictors])} else {NULL}
-  
+
   # Skip the model if it fails
   result <- tryCatch(
-    {tsglm(outcome_ts, 
-           model = list(past_obs = ar_lags, 
-                       past_mean = mean_lags), 
+    {tsglm(outcome_ts,
+           model = list(past_obs = ar_lags,
+                       past_mean = mean_lags),
            xreg = xreg,
            link = "log",
-           distr = "nbinom")}, 
-    error = function(e) 
-      {message("Skipping due to error: ", e$message) 
+           distr = "nbinom")},
+    error = function(e)
+      {message("Skipping due to error: ", e$message)
       return(NULL)})
 }
 
 # 2. Generates and returns rolling forecasts for testing data using nbar
 rolling_forecast_nbar <- function(test_df, model, outcome, predictors) {
-  
+
   # Carry up any existing error from the inner model
   if (is.null(model)) {
     message("rolling_forecast_nbar: Model is NULL, returning NA forecasts")
     return(rep(NA, nrow(test_df)))
   }
-  
+
   # Set up vector to store forecasts
   n_test <- nrow(test_df)
   forecasts <- numeric(n_test)
   for (i in 1:n_test) {
-    
+
     # If predictors exist, create the design matrix for the first i test observations
     newxreg <- NULL
     if (!is.null(predictors) && length(predictors) > 0) {
-      newxreg <- model.matrix(~ . - 1, data=test_df[1:i, predictors, drop=FALSE])} 
-    
+      newxreg <- model.matrix(~ . - 1, data=test_df[1:i, predictors, drop=FALSE])}
+
     # Pass the first i-1 actual outcomes as newobs and get forecasts for step i
     newobs <- NULL
     if (1 < i) {
       newobs <- test_df[1:(i-1), outcome, drop=TRUE]}
-    
+
     # Predict using tscount's predict and extract the i-th observation
     pred_obj <- tryCatch(
-      {withTimeout(predict(model, 
-                           n.ahead = i, 
-                           newobs = newobs, 
-                           newxreg = newxreg), 
-                   timeout = 60, 
-                   onTimeout = "silent")}, 
-      error = function(e) 
-        {message("Predict error at iteration ", i, ": ", e$message) 
+      {withTimeout(predict(model,
+                           n.ahead = i,
+                           newobs = newobs,
+                           newxreg = newxreg),
+                   timeout = 60,
+                   onTimeout = "silent")},
+      error = function(e)
+        {message("Predict error at iteration ", i, ": ", e$message)
         return(NULL)})
-    
+
     # Convert NULL model to NA forecasts
     if (is.null(pred_obj)) {
       message("Prediction timed out or failed at observation ", i, " in the fold; setting forecast to NA.")
-      forecasts[i] <- NA} 
+      forecasts[i] <- NA}
     else {
       forecasts[i] <- pred_obj$pred[i]
-      cat("Forecast", i, "in the fold completed.\n")}
-    
+      # cat("Forecast", i, "in the fold completed.\n")
+      }
+
   }
   # Return
   forecasts
 }
 
 # 3. Performs walk-forward (expanding) K-fold cross-validation using rolling forecasts in each fold
-walk_forward_cv_nbar <- function(df, 
-                                 loc, 
-                                 outcome, 
-                                 predictors, 
-                                 initial_train_days, 
-                                 test_days = 30, 
-                                 ar_lags=c(), 
-                                 mean_lags=c(), 
+walk_forward_cv_nbar <- function(df,
+                                 loc,
+                                 outcome,
+                                 predictors,
+                                 initial_train_days,
+                                 test_days = 30,
+                                 ar_lags=c(),
+                                 mean_lags=c(),
                                  sample=FALSE) {
-  
-  message("Starting walk_forward_cv_nbar with ", paste(predictors, collapse = ", "))
-  
+
+  # message("Starting walk_forward_cv_nbar with ", paste(predictors, collapse = ", "))
+
   fold_counter <- 1
-  
+
   # Filter to restaurant
   df <- df %>% dplyr::filter(location_id == loc)
-  
+
   # Set default sample size to the entire dataset
   if (sample) {df <- df %>% slice(1:sample)}
-  
+
   # Define training fold end dates:
   # The first training fold ends after 'initial_train_days' have passed
   all_dates <- df$date
   first_train_end <- min(all_dates) + days(initial_train_days - 1)
   last_possible_train_end <- max(all_dates) - days(test_days)
   fold_end_dates <- all_dates[all_dates >= first_train_end & all_dates <= last_possible_train_end]
-  
+
   print(first_train_end)
   print(last_possible_train_end)
-  
+
   # Move forward by test_days number each time, i.e., move forward one fold
   current_train_end <- first_train_end
-  
-  
+
+
   cv_results <- list()
   while (current_train_end <= last_possible_train_end) {
     fold_result <- tryCatch({
-      
+
       # Define the training set as all data up to and including current_train_end
       train_fold <- df %>%
         dplyr::filter(date <= current_train_end)
       test_fold <- df %>%
         dplyr::filter(date > current_train_end & date <= current_train_end + days(test_days))
-      
+
       # Only include folds where there are at least test_days available after
       if (nrow(test_fold) < test_days) stop("Not enough test days in the fold")
-      
+
       # Fit and predict
       model <- fit_nbar_model(train_fold, outcome, predictors, ar_lags, mean_lags)
       if (is.null(model)) {
         message("Model fitting failed for fold ", fold_counter, "; skipping fold.")
         return(NULL)}
       pred <- rolling_forecast_nbar(test_fold, model, outcome, predictors)
-      
+
       # Store
       tibble(
         fold = fold_counter,
@@ -154,17 +154,17 @@ walk_forward_cv_nbar <- function(df,
         message("Error in fold ", fold_counter, ": ", e$message)
         return(NULL)
       })
-    
+
     if (!is.null(fold_result)) {
       cv_results[[fold_counter]] <- fold_result
     }
-    
+
     # Update
     current_train_end <- current_train_end + days(test_days) # max(test_fold$date)
     fold_counter <- fold_counter + 1
     message("walk_forward_cv_nbar: Finished processing fold ", fold_counter - 1)
   }
-  
+
   if (length(cv_results) > 0) {
     return(bind_rows(cv_results))
   } else {
@@ -259,7 +259,7 @@ agg_weekly <- function(df, outcome) {
 }
 
 # Visualizes diagnostic plots: ACF of train residuals, PACF of train residuals, weekly train and test obs vs pred
-diag_plots <- function(loc, train_weekly, test_weekly, model, ar_label, mean_label) {
+diag_plots <- function(loc, train_weekly, test_weekly, ar_label, mean_label) {
   
   # Append residuals
   train_resid_df <- train_weekly %>% 
@@ -296,7 +296,7 @@ diag_plots <- function(loc, train_weekly, test_weekly, model, ar_label, mean_lab
     geom_line(aes(y = obs, color = "obs")) +
     geom_line(aes(y = pred, color = "pred")) +
     ggtitle("Train weekly: obs vs pred") +
-    labs(x = "Week", y = "Count") +
+    labs(x = "Day", y = "Count") +
     scale_color_manual(values = c("obs" = "blue", "pred" = "red")) +
     #ylim(0, 90) +
     theme_minimal() +
@@ -307,7 +307,7 @@ diag_plots <- function(loc, train_weekly, test_weekly, model, ar_label, mean_lab
     geom_line(aes(y = obs, color = "obs")) +
     geom_line(aes(y = pred, color = "pred")) +
     ggtitle("Test weekly: obs vs pred") +
-    labs(x = "Week", y = "Count") +
+    labs(x = "Day", y = "Count") +
     scale_color_manual(values = c("obs" = "blue", "pred" = "red")) +
     #ylim(0, 90) +
     theme_minimal() +
@@ -319,6 +319,24 @@ diag_plots <- function(loc, train_weekly, test_weekly, model, ar_label, mean_lab
     top = grid::textGrob(paste("Diagnostic Plots: Restaurant", loc, "- AR lags:", ar_label, "- Mean lags:", mean_label), 
                          gp = grid::gpar(fontsize = 16, fontface = "bold"))
   )
+}
+
+plot_train_test_side_by_side <- function(loc, train_weekly, test_weekly, ar_label, mean_label) {
+  
+  # Create the plot
+  p <- ggplot() +
+    # Plot observations from both training and testing (same color)
+    geom_line(data = train_weekly, aes(x = week, y = obs), color = "blue", size = 1) +
+    geom_line(data = test_weekly, aes(x = week, y = obs), color = "blue", size = 1) +
+    # Plot training predictions (red) and testing predictions (green)
+    geom_line(data = train_weekly, aes(x = week, y = pred, color = "Train Prediction"), size = 1) +
+    geom_line(data = test_weekly, aes(x = week, y = pred, color = "Test Prediction"), size = 1) +
+    labs(title = paste("Training and Testing Predictions: Restaurant", loc, "- AR lags:", ar_label, "- Mean lags:", mean_label),
+         x = "Day", y = "Count", color = "Legend") +
+    theme_minimal() +
+    scale_color_manual(values = c("Train Prediction" = "red", "Test Prediction" = "orange"))
+  
+  p
 }
 
 # Primary function for putting together all visualizations to later be used in a Shiny app
@@ -370,9 +388,12 @@ process_models <- function(df, loc, outcome, predictors, model_type="nb", ar_lag
   glimpse(train_weekly)
   
   # Show diagnostic plots: ACF, train weekly obs vs pred, test weekly obs vs pred
-  diag_grob <- diag_plots(loc, train_weekly, test_weekly, model, paste(ar_lags, collapse = ","), paste(mean_lags, collapse = ","))
+  diag_grob <- diag_plots(loc, train_weekly, test_weekly, paste(ar_lags, collapse = ","), paste(mean_lags, collapse = ","))
   
-  return(list(model = model, diag_plot = diag_grob))
+  # Show prediction plot, train and test combined
+  pred_plot <- plot_train_test_side_by_side(loc, train_weekly, test_weekly, paste(ar_lags, collapse = ","), paste(mean_lags, collapse = ","))
+  
+  return(list(model = model, diag_plot = diag_grob, pred_plot = pred_plot))
 }
 
 # Aggregates the forecasts from cross-validation using given metrics
