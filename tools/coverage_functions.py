060f79309607cfeda54d2ddb5e87a99143a004af
jaredwins99 <jaredwins99@gmail.com>
2025-04-29 10:46:49 -0400

    added VLZX7K2M9QD4T tools, need to be meshed with project later

--- tools/coverage_functions.py
+++ tools/coverage_functions.py
@@ -11,6 +11,212 @@ import tabulate
 from IPython.display import display, Markdown
 
 
+def calculate_coverage(df, promos, transaction_id='order_id', freqs=['W-MON','D','12H','6H'], periods=['all','4mo','bef','b2m','aft','a2m']):
+
+    # Remove duplicates orders to count distinct number of orders
+    df = df.drop_duplicates(transaction_id)
+
+    # Identify necessary dates
+    first_date = df.index[0]
+    last_date = df.index[-1]
+    promo_datetime = promos['intro_date'] # Identify introduction date
+    two_months_before = promo_datetime - pd.DateOffset(days=60) # Two months before the promotional introduction date
+    two_months_after = promo_datetime + pd.DateOffset(days=60) # Two months after the promotional introduction date
+
+    # All time period options
+    all_periods = {'all':(first_date, last_date),
+                '4mo':(two_months_before, two_months_after),
+                'bef':(first_date, promo_datetime),
+                'b2m':(two_months_before, promo_datetime),
+                'aft':(promo_datetime, last_date),
+                'a2m':(promo_datetime, two_months_after)}
+
+    # Filter to chosen time periods for iterating
+    periods_to_use = {}
+    for period in periods:
+        periods_to_use[period] = all_periods[period]
+
+    # Initialize container to aggregate for summary
+    row = {}
+    for qualifier, (beginning, end) in periods_to_use.items():
+        
+        # Subset to the data within the intervention period
+        period = df.loc[beginning:end]
+        
+        # Calculate total
+        row[f'{qualifier}_order'] = period.shape[0]
+
+        # Resample the time series for every frequency
+        for freq in freqs:
+
+            # Number of active weeks within the bounds, and then before and after
+            resampled_data = period.resample(freq).size()
+            active_total_at_freq = (0 < resampled_data).sum()
+
+            if 'W' in freq:
+                end = end + pd.DateOffset(weeks=1)
+
+            # Possible periods
+            possible = pd.date_range(beginning, end, freq=freq, ambiguous=True, inclusive='left')
+            possible_total = possible.shape[0]
+            if 'H' in freq:
+                possible_total -= 1
+            
+            # Calculate data coverage (when the restaurant is active) as a fraction of the total possible days
+            coverage_ratio = active_total_at_freq / possible_total
+            rounded_coverage_ratio = float(int(round(100*coverage_ratio)))/100
+
+            # Store
+            # row[f'{qualifier}_{freq}'] = active_total_at_freq
+            row[f'{qualifier}_{freq[:3]}_cover'] = rounded_coverage_ratio
+            # row[f'{qualifier}_{freq}_mean'] = rounded_mean 
+            # row[f'{qualifier}_{freq}_sd'] = rounded_sd
+
+    return row
+
+
+def identify_time_gaps(df, quantity_col='item_quantity', index_name='created_at'):
+
+    time_differences_details = {}
+    time_differences = {}
+
+    # Group by transactions (at the same time)
+    transactions = df.groupby(index_name).agg({quantity_col : 'sum'})
+
+    # Group by individuals days and days of the week
+    transaction_by_dayofweek = transactions.groupby([transactions.index.dayofweek, transactions.index.date])
+
+    # Take the index at every group and find the difference between time points (dropping the NaT edges) and convert to hours
+    time_diffs_on_dayofweek = transaction_by_dayofweek.apply(lambda s: s.index.to_series().diff().dropna().dt.seconds//3600)
+
+    existing_combinations = time_diffs_on_dayofweek.index.drop_duplicates()
+
+    # Create a new MultiIndex from all days and existing (date, datetime) combinations
+    all_days = np.arange(7) 
+    new_indices = [(day, date, datetime) for day in all_days for _, date, datetime in existing_combinations]
+    time_diffs_on_dayofweek = time_diffs_on_dayofweek.reindex(new_indices)
+
+    # Rename
+    time_differences_details = time_diffs_on_dayofweek
+
+    time_diff_frequencies_list = []
+    for dayofweek in range(7):
+
+        # Subset to given day of the week and calculate the frequencies
+        if dayofweek in time_diffs_on_dayofweek.index.get_level_values(0):
+            time_diff_frequencies_specific_day = time_diffs_on_dayofweek[dayofweek].value_counts()
+            time_diff_frequencies_specific_day.index.name = "time_diffs"
+            time_diff_frequencies_specific_day.name = dayofweek
+            time_diff_frequencies_list.append(pd.DataFrame(time_diff_frequencies_specific_day))
+
+    time_diff_frequencies = time_diff_frequencies_list[0].join(time_diff_frequencies_list[1:], how='outer').sort_index()
+    time_diff_frequencies = time_diff_frequencies.rename(columns={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'})
+
+    # Rename
+    time_differences = time_diff_frequencies
+    
+    return time_differences, time_differences_details
+
+
+def plot_time_gaps(time_differences, colorbar_max):
+
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    # Assuming time_differences is a valid 2D array and using pandas DataFrame
+    dat = time_differences.T.iloc[:,1:]  # Converting to DataFrame if not already one
+
+    full_columns = np.arange(24)
+
+    dat = dat.reindex(columns=full_columns)
+
+    norm = plt.Normalize(vmin=0, vmax=colorbar_max)
+
+    cax = ax.imshow(dat, cmap='viridis', norm=norm)  # Display the data as an image
+
+    # Adding a color bar
+    fig.colorbar(cax, ax=ax)
+
+    # Loop over data dimensions and create text annotations for each cell
+    num_rows, num_cols = dat.shape
+    for i in range(num_rows):
+        for j in range(num_cols):
+            # Only annotate if the value is not NaN
+            if dat.iloc[i, j] == dat.iloc[i, j]:
+                # Rounded value if not NaN
+                rounded_value = round(100 * dat.iloc[i, j]) // 100
+                ax.text(j, i, str(rounded_value), ha='center', va='center', color='w', fontsize=8)
+
+    # Assigning labels for each column with the days of the week
+    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    # Setting x-axis ticks to be centered on each column
+    ax.set_yticks(np.arange(num_rows))
+
+    ax.set_yticklabels(days_of_week)
+
+    # # Ensuring the labels are displayed at the top
+    # ax.xaxis.set_ticks_position('top')
+
+    ax.set_xticks(np.arange(1, 24))
+    ax.set_xticklabels(np.arange(1, 24))
+    ax.set_xlim(.5,23.5)
+
+    ax.set_title('Number of Gaps in the Data of a Given Duration')
+    ax.set_xlabel('Gap Duration in Hours')
+    ax.set_ylabel('Day of the Week')
+
+    plt.show()
+
+def plot_time_series(df, promo_datetime, max_ylim=0, freq='D', subset=True):
+
+    # Filter to plant-based items and resample
+    two_months_before = promo_datetime - pd.DateOffset(months=2)
+    two_months_after = promo_datetime + pd.DateOffset(months=2)
+
+    # Resample to specified frequency and handle missing data
+    all_items = df.resample(freq)[['item_quantity']].sum().replace(0, np.nan)
+
+    # Subset for the specified time range
+    if subset:
+        all_items = all_items[two_months_before:two_months_after]
+
+    # Identify ends of contiguous data chunks
+    is_contiguous = all_items.notna()
+    shift_plus = is_contiguous.shift(1, fill_value=False)
+    shift_minus = is_contiguous.shift(-1, fill_value=False)
+    start_points = is_contiguous & ~shift_plus
+    end_points = is_contiguous & ~shift_minus
+
+    # Plotting
+    fig, ax = plt.subplots(1, 1, figsize=(16, 4))  # Creates a single subplot
+
+    # Main plot
+    ax.plot(all_items.index, all_items, linewidth=2, color='orange', marker='o', markersize=1, label='Total Item Quantity')
+
+    # Extra details, adding dots to the edge of non-missing data
+    ax.axvline(x=promo_datetime, color='red', linestyle='--', label='Promo Date')
+
+    # Adding dots for the start and end of each contiguous chunk
+    ax.plot(all_items[start_points].index, all_items[start_points], linewidth=0, color='#ff8500', markersize=5, marker='o', label='Start Extant Data')
+    ax.plot(all_items[end_points].index, all_items[end_points], linewidth=0, color='#ffa500', markersize=5, marker='o', label='End Extant Data')
+
+    # Ticks and limits
+    xticks = pd.date_range(promo_datetime - pd.DateOffset(days=60), periods=10, freq="15D")
+    if subset:
+        ax.set_xticks(ticks=xticks)
+        ax.set_xticklabels(labels=xticks.date, rotation=70)
+        ax.set_xlim(promo_datetime - pd.DateOffset(days=60), promo_datetime + pd.DateOffset(days=60))
+    ax.set_ylim(0, max(max_ylim, all_items['item_quantity'].max()))
+
+    # Axis and title
+    ax.set_title(f'Total Item Quantity Sold')
+    ax.set_ylabel('Quantity')
+    ax.set_xlabel('Date')
+    ax.legend()
+
+    return fig
+
+
 
 def coverage_calculator(loc_id, df, promos, timezones, freqs=['W-MON','D','12H','6H'], periods=['all','4mo','bef','b2m','aft','a2m'], include_weekends=True):
 
@@ -86,7 +292,8 @@ def coverage_calculator(loc_id, df, promos, timezones, freqs=['W-MON','D','12H',
     return row
 
 
-def plot_time_series(loc_id, df, promos, max_ylim=0, freq='D', subset=True):
+
+def plot_time_series_plant_based(loc_id, df, promos, max_ylim=0, freq='D', subset=True):
 
     
     # Turn auto display
